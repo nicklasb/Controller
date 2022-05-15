9c18ee8ec2fdcc782e3e390883006964950a181b
Nicklas Börjesson <nicklas@optimalbpm.se>
2022-05-15 16:41:33 +0200

    Add TODO:s and comments

--- lib/ble/sdp_monitor.h
+++ lib/ble/sdp_monitor.h
@@ -3,11 +3,21 @@ extern "C"
 {
 #endif
 /**
- * This is the monitoring of the Sensor Data Protocol (SDP) implementation
+ * This is a monitor that periodically samples different aspects at runtime.
+ * It is used for reporting and detecting and reporting problems, but also for .
+ * troubleshooting (especially memory leaks) and optimization.
+ * TODO: Question: Should the monitor also be the one that gets samples for the controller?
+ * Or is that out of scope? Should that more be a scheduler?
  */
+
+struct monitor {
+    problem
+}
 
 
 void init_monitor(char *log_prefix);
+
+int add_monitor()
 
 #ifdef __cplusplus
 } /* extern "C" */

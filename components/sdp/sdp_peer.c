3abd1022ab7505f63b681974e57ba766bf44cc70
Nicklas Börjesson <nicklasb@gmail.com>
2022-10-08 14:55:15 +0200

    Add named peers

--- components/sdp/sdp_peer.c
+++ components/sdp/sdp_peer.c
@@ -1,12 +1,40 @@
 #include "sdp_peer.h"
-#include "sdp.h"
-#include <os/os_mempool.h>
 
-static void *peer_mem;
+#include <os/os_mempool.h>
+#include <esp_log.h>
+
+#include "sdp.h"
+
+#ifdef CONFIG_SDP_LOAD_BLE
+    #include "ble/ble_spp.h"
+#endif    
+
+
+/* */
+static void *sdp_peer_mem;
 static struct os_mempool sdp_peer_pool;
 
-struct sdp_peer *
-peer_find(__uint16_t peer_handle)
+/* Used for creating new peer handles*/
+__uint16_t _peer_handle_incrementor_ = -1;
+
+struct sdp_peer*
+sdp_peer_find_name(const sdp_peer_name name)
+{
+    struct sdp_peer *peer;
+
+    SLIST_FOREACH(peer, &sdp_peers, next)
+    {
+        if (strcmp(peer->name, name) != 0)
+        {
+            return peer;
+        }
+    }
+
+    return NULL;
+}
+
+struct sdp_peer*
+sdp_peer_find_handle(__int16_t peer_handle)
 {
     struct sdp_peer *peer;
 
@@ -21,36 +49,42 @@ peer_find(__uint16_t peer_handle)
     return NULL;
 }
 
-
-int peer_delete(__uint16_t peer_handle)
+int sdp_peer_delete(__uint16_t peer_handle)
 {
     struct sdp_peer *peer;
     int rc;
 
-    peer = peer_find(peer_handle);
+    peer = sdp_peer_find_handle(peer_handle);
     if (peer == NULL)
     {
         return SDP_ERR_PEER_NOT_FOUND;
     }
 
-    SLIST_REMOVE(&sdp_peers, peer, sdp_peer, next);
+    #ifdef CONFIG_SDP_LOAD_BLE
+        // If connected,remove BLE peer
+        if (peer->ble_conn_handle != 0) {
+            ble_peer_delete(peer->ble_conn_handle);
+        }
+    #endif 
 
+    SLIST_REMOVE(&sdp_peers, peer, sdp_peer, next);
 
     rc = os_memblock_put(&sdp_peer_pool, peer);
     if (rc != 0)
     {
         return SDP_ERR_OS_ERROR;
     }
+ 
 
     return 0;
 }
 
-int peer_add(__uint16_t peer_handle, const peer_name name)
+int sdp_peer_add(const sdp_peer_name name)
 {
     struct sdp_peer *peer;
 
-    /* Make sure the connection handle is unique. */
-    peer = peer_find(peer_handle);
+    /* TODO: Make sure the peer name is unique*/
+    peer = sdp_peer_find_name(name);
     if (peer != NULL)
     {
         return SDP_ERR_PEER_EXISTS;
@@ -62,41 +96,44 @@ int peer_add(__uint16_t peer_handle, const peer_name name)
         /* Out of memory. */
         return SDP_ERR_OUT_OF_MEMORY;
     }
-
     memset(peer, 0, sizeof *peer);
-    peer->peer_handle = peer_handle;
-    memcpy(&peer->name, &name, sizeof name);
+    peer->peer_handle = _peer_handle_incrementor_++;
+    strcpy(peer->name, name);
 
     SLIST_INSERT_HEAD(&sdp_peers, peer, next);
 
-    return 0;
+    return peer->peer_handle;
 }
 
 static void
-peer_free_mem(void)
+sdp_peer_free_mem(void)
 {
-    free(peer_mem);
-    peer_mem = NULL;
+    free(sdp_peer_mem);
+    sdp_peer_mem = NULL;
 
 }
 
-int peer_init(int max_peers)
+int sdp_peer_init(int max_peers)
 {
     int rc;
 
-    /* Free memory first in case this function gets called more than once. */
-    peer_free_mem();
 
-    peer_mem = malloc(
+
+    /* Free memory first in case this function gets called more than once. */
+    sdp_peer_free_mem();
+
+    sdp_peer_mem = malloc(
     OS_MEMPOOL_BYTES(max_peers, sizeof(struct sdp_peer)));
-    if (peer_mem == NULL)
+    if (sdp_peer_mem == NULL)
     {
         rc = SDP_ERR_OUT_OF_MEMORY;
         goto err;
     }
 
+
+
     rc = os_mempool_init(&sdp_peer_pool, max_peers,
-                         sizeof(struct sdp_peer), peer_mem,
+                         sizeof(struct sdp_peer), sdp_peer_mem,
                          "peer_pool");
     if (rc != 0)
     {
@@ -104,10 +141,9 @@ int peer_init(int max_peers)
         goto err;
     }
 
-
     return 0;
 
 err:
-    peer_free_mem();
+    sdp_peer_free_mem();
     return rc;
 }
